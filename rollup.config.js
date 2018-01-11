94db403d1f29767b8f716294da0300537c9113f2
Michael Lin <unadlib@gmail.com>
2018-01-11 15:37:05 +0800

    update

--- rollup.config.js
+++ rollup.config.js
@@ -10,7 +10,7 @@ export default {
   output: {
     file: `dist/index.js`,
     format: 'umd',
-    name: pgk.name,
+    name: 'reactIflow',
     sourcemap: true,
     globals: {
       'react': 'React',
